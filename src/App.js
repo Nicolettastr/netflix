099efe0970db1878a5fa0a41473302715047985c
Nicolettastr <nicolettastruggia@hotmail.com>
2023-03-13 19:10:20 +0100

    trying to fix switch problems

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import HomeScreen from './components/HomeScreen.jsx'
+import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
     <div className="app">
-      <HomeScreen/>
+      <Router>
+        <Switch>
+          <Route path="/">
+            <HomeScreen />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
--- src/components/banner.jsx
+++ src/components/banner.jsx
@@ -1,25 +1,48 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import '../css/banner.css'
+import axios from './axios'
+import requests from "../Requests";
 
 const Banner = () => {
+
+    const [movie, setMovie] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const request = await axios.get(requests.fetchNetflixOriginals);
+            setMovie(request.data.results[
+                Math.floor(Math.random() * request.data.results.length - 1)
+            ]
+            );
+            return request;
+        }
+        fetchData();
+    }, [])
+
+    console.log(movie)
+
+    const truncate = (string, n) => {
+        return string?.length > n ? string.substr(0, n - 1) + '...' : string
+    }
+
     return (
         <header className="banner" style={{
             backgroundSize: 'cover',
-            backgroundImage: `url('https://d1csarkz8obe9u.cloudfront.net/posterpreviews/netflix-entertainment-poster-template-design-7bcb489fc724e24ae5918a915769aecc_screen.jpg?ts=1587213240')`,
+            backgroundImage: `url('https://image.tmdb.org/t/p/original/${movie?.backdrop_path}')`,
             backgroundPosition: 'center center',
         }}>
 
             <div className="banner_content">
-                <h1 className="banner_title"> Movie Name</h1>
+                <h1 className="banner_title">{movie?.title || movie?.name || movie?.original_name}</h1>
                 <div className="banner_buttons">
                     <button className="banner_button">Play</button>
                     <button className="banner_button">My list</button>
                 </div>
                 <h1 className="banner_description">
-                    this is a test description
+                    {truncate(movie?.overview, 160)}
                 </h1>
             </div>
-            <div className="banner--fadeButton"/>
+            <div className="banner--fadeButton" />
         </header>
     )
 };
